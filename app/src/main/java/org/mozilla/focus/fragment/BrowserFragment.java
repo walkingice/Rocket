c6af9beedb64366f64cef63f8b27f1038ce4c872
Julian_Chu <walkingice0204@gmail.com>
2018-06-03 11:38:19 +0800

    To make gecko view work, by changing attach, detach

--- app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
+++ app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
@@ -451,8 +451,8 @@ public class BrowserFragment extends LocaleAwareFragment implements View.OnClick
             if (tabView == null) {
                 return;
             }
-            current.detach();
-            webViewSlot.removeView(tabView.getView());
+            //current.detach();
+            //webViewSlot.removeView(tabView.getView());
         }
     }
 
@@ -464,7 +464,9 @@ public class BrowserFragment extends LocaleAwareFragment implements View.OnClick
                 return;
             }
             final View inView = tabView.getView();
-            webViewSlot.addView(inView);
+            if (inView.getParent() == null) {
+                webViewSlot.addView(inView);
+            }
         }
     }
 
@@ -901,9 +903,9 @@ public class BrowserFragment extends LocaleAwareFragment implements View.OnClick
             if (current == null) {
                 return;
             }
-            WebBackForwardList webBackForwardList = ((WebView) current).copyBackForwardList();
-            WebHistoryItem item = webBackForwardList.getItemAtIndex(webBackForwardList.getCurrentIndex() - 1);
-            updateURL(item.getUrl());
+//            WebBackForwardList webBackForwardList = ((WebView) current).copyBackForwardList();
+//            WebHistoryItem item = webBackForwardList.getItemAtIndex(webBackForwardList.getCurrentIndex() - 1);
+//            updateURL(item.getUrl());
             current.goBack();
         }
     }
@@ -915,9 +917,9 @@ public class BrowserFragment extends LocaleAwareFragment implements View.OnClick
             if (current == null) {
                 return;
             }
-            WebBackForwardList webBackForwardList = ((WebView) current).copyBackForwardList();
-            WebHistoryItem item = webBackForwardList.getItemAtIndex(webBackForwardList.getCurrentIndex() + 1);
-            updateURL(item.getUrl());
+//            WebBackForwardList webBackForwardList = ((WebView) current).copyBackForwardList();
+//            WebHistoryItem item = webBackForwardList.getItemAtIndex(webBackForwardList.getCurrentIndex() + 1);
+//            updateURL(item.getUrl());
             current.goForward();
         }
     }
@@ -1261,16 +1263,7 @@ public class BrowserFragment extends LocaleAwareFragment implements View.OnClick
             if (tabView == null) {
                 throw new RuntimeException("Tabview should be created at this moment and never be null");
             }
-            // ensure it does not have attach to parent earlier.
-            targetTab.detach();
-
-            @Nullable final View outView = findExistingTabView(webViewSlot);
-            webViewSlot.removeView(outView);
-
-            final View inView = tabView.getView();
-            webViewSlot.addView(inView);
-
-            startTransitionAnimation(null, inView, null);
+            targetTab.getTabView().onAttach(webViewSlot);
         }
 
         private void refreshChrome(Tab tab) {
