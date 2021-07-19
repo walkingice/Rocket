632b2e99156ee4ece2d85fa6be51ba293cb25135
Julian_Chu <walkingice0204@gmail.com>
2021-07-20 01:59:59 +0900

    import Parcelize from kotlin-parcelize package

--- app/src/main/java/org/mozilla/rocket/content/common/data/TabSwipeTelemetryData.kt
+++ app/src/main/java/org/mozilla/rocket/content/common/data/TabSwipeTelemetryData.kt
@@ -1,7 +1,7 @@
 package org.mozilla.rocket.content.common.data
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class TabSwipeTelemetryData(
